ab04832c4169a72aa6c573bc425dc06995857577
Alex Aguinaldo <psionicbones@gmail.com>
2016-11-28 11:35:46 -1000

    update

---
+++ loopscity.js
@@ -0,0 +1,18 @@
+// Mommy I can count to 100
+var count = 0;
+for (var count = 0; count <= 100; count++) {
+	console.log("Yan can count to " + count);
+}
+
+// Noah's Arc
+var animalCount = 0;
+for (var animalCount = 0; animalCount <= 5; animalCount++) {
+	console.log("There are " + animalCount + " many animals on Noah's Arc.");
+}
+
+// Hip Hop Array
+var partyAnimals = ["Penguin", "Lion", "Dolphin", "Panther", "Emu"];
+for (var partyAnimals)
+
+// Merlin's Battle Weapon
+var 
